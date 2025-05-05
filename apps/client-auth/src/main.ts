4503f8aad196cae8c5e25619ab46e992385b999f
Iscream-Rick <rick.kim@i-screamarts.com>
2025-05-05 21:47:24 +0900

    Updated main.ts

--- apps/client-auth/src/main.ts
+++ apps/client-auth/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { ClientAuthModule } from './client-auth.module';
+import { ValidationPipe } from '@nestjs/common/pipes';
+import { ServiceExceptionFilter } from '@app/exception/filter/service.exception.filter';
+import { LogInterceptor } from '@app/interceptor/log.interceptor';
+import { ZodValidationPipe } from 'nestjs-zod';
 
 async function bootstrap() {
   const app = await NestFactory.create(ClientAuthModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
+  app.useGlobalFilters(new ServiceExceptionFilter());
+  app.useGlobalPipes(new ZodValidationPipe());
+  app.useGlobalInterceptors(new LogInterceptor());
   await app.listen(process.env.port ?? 3000);
 }
-bootstrap();
+void bootstrap();
