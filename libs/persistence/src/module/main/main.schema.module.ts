459dedf64ebec9203e65c537a12ace609c52959c
Iscream-Rick <rick.kim@i-screamarts.com>
2025-05-06 18:37:56 +0900

    Registered repository to the module

--- libs/persistence/src/module/main/main.schema.module.ts
+++ libs/persistence/src/module/main/main.schema.module.ts
@@ -2,9 +2,23 @@ import { mainConnection } from '@app/persistence/persistence.connection.main';
 import { Module } from '@nestjs/common';
 import { ClientKeypairRepository } from './repository/client.keypair.repository';
 import { MemberRepository } from './repository/member.repository';
+import { MemberDetailRepository } from './repository/member.detail.repository';
+import { MemberPhoneRepository } from './repository/member.phone.repository';
 
 @Module({
-  providers: [mainConnection, ClientKeypairRepository, MemberRepository],
-  exports: [mainConnection, ClientKeypairRepository, MemberRepository],
+  providers: [
+    mainConnection,
+    ClientKeypairRepository,
+    MemberRepository,
+    MemberDetailRepository,
+    MemberPhoneRepository,
+  ],
+  exports: [
+    mainConnection,
+    ClientKeypairRepository,
+    MemberRepository,
+    MemberDetailRepository,
+    MemberPhoneRepository,
+  ],
 })
 export class MainSchemaModule {}
