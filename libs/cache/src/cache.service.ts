31a1828505d3308b484437039825951a876579b7
Iscream-Rick <rick.kim@i-screamarts.com>
2025-05-11 14:38:38 +0900

    Added functionalities for the sign in

--- libs/cache/src/cache.service.ts
+++ libs/cache/src/cache.service.ts
@@ -1,12 +1,17 @@
 import { Inject, Injectable } from '@nestjs/common';
-import { GlideClient } from '@valkey/valkey-glide';
+import { GlideClient, Transaction } from '@valkey/valkey-glide';
 import { VALKEY_CONNECTION } from './cache-connection-symbol';
 
 @Injectable()
 export class CacheService {
   protected cache: GlideClient;
+  protected transaction: Transaction;
 
   constructor(@Inject(VALKEY_CONNECTION) _cache: GlideClient) {
     this.cache = _cache;
+  }
+
+  getTransaction() {
+    this.transaction = new Transaction();
   }
 }
--- libs/cache/src/repository/passport.cache.repository.ts
+++ libs/cache/src/repository/passport.cache.repository.ts
@@ -2,26 +2,41 @@ import { Injectable } from '@nestjs/common';
 import { CacheService } from '@app/cache/cache.service';
 import {
   Decoder,
-  GlideReturnType,
+  DecoderOption,
   GlideString,
+  SetOptions,
   TimeUnit,
+  Transaction,
 } from '@valkey/valkey-glide';
 import { CacheResponseEntity } from '../entity/cache.response.entity';
 
 @Injectable()
 export class PassportCacheRepository extends CacheService {
+
+  private createSetOption(): SetOptions {
+    return {
+      expiry: { type: TimeUnit.Milliseconds, count: 1000 * 60 * 60 },
+      conditionalSet: 'onlyIfDoesNotExist',
+      returnOldValue: false,
+    };
+  }
+
   async setPassport(
     key: string,
     data: string,
   ): Promise<CacheResponseEntity<string>> {
-    const result = await this.cache.set(key, data, {
-      expiry: { type: TimeUnit.Milliseconds, count: 1000 * 60 * 60 },
-      conditionalSet: 'onlyIfDoesNotExist',
-      returnOldValue: false,
-    });
+    const result = await this.cache.set(key, data, this.createSetOption());
     if (result === 'OK')
       return new CacheResponseEntity<string>(true, result.toString());
     return new CacheResponseEntity<string>(false, result?.toString());
+  }
+
+  setPassportWithTransaction(
+    transaction: Transaction,
+    key: string,
+    data: string,
+  ): Transaction {
+    return transaction.set(key, data, this.createSetOption());
   }
 
   async getPassport(
@@ -30,5 +45,12 @@ export class PassportCacheRepository extends CacheService {
     const result = await this.cache.get(key, { decoder: Decoder.String });
     if (result) return new CacheResponseEntity<string>(true, result.toString());
     return new CacheResponseEntity<string>(false);
+  }
+
+  getPassportWithTransaction(
+    transaction: Transaction,
+    key: string,
+  ): Transaction {
+    return transaction.get(key);
   }
 }
